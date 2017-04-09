f932ba8e81046a7bea55a9c1eb01eb76ee057420
Sergiy Kapustin <herr.kapustin@outlook.com>
2017-04-09 14:50:43 +0200

    no message

--- main-webapp/src/main/webapp/js/index.js
+++ main-webapp/src/main/webapp/js/index.js
@@ -1,6 +1,5 @@
-function openDoor() {
-        
-		document.getElementById("door1").class='activeSensor';
+function changeState(idText) {
+		alert(idText);
 }
 
 var app = angular.module('floorplan', []);
@@ -8,12 +7,7 @@ var app = angular.module('floorplan', []);
 app.controller('SensorLayoutCtrl', function($scope) {
   
 $scope.roomTitle = "First floor";  
-
-$scope.room1Outline = "M0 0 L0 150 L400 150 L400 0 Z"
-$scope.room2Outline = "M0 150 L0 300 L400 300 L400 150 Z"
-$scope.room3Outline = "M0 300 L0 450 L400 450 L400 300 Z"
   
-
    $scope.rotateSensor = function (sensorRotation) {
     var degrees = 0;
       if(sensorRotation != null) degrees = sensorRotation;
@@ -25,26 +19,41 @@ $scope.room3Outline = "M0 300 L0 450 L400 450 L400 300 Z"
  
   $scope.sensors = [
     
-{compId:"stovetop1",room:"Room1",sNum:1,sWidth:40,sHeight:40,x:0,y:110,rotate:null},
-{compId:"pills1",room:"Room1",sNum:2,sWidth:20,sHeight:20,x:100,y:50,rotate:null},
-{compId:"alarm1",room:"Room1",sNum:3,sWidth:20,sHeight:20,x:150,y:50,rotate:null},
-{compId:"door1",room:"Room1",sNum:4,sWidth:70,sHeight:5,x:395,y:110,rotate:-90},
-{compId:"stovetop2",room:"Room2",sNum:5,sWidth:40,sHeight:40,x:0,y:260,rotate:null},
-{compId:"pills2",room:"Room2",sNum:6,sWidth:20,sHeight:20,x:100,y:200,rotate:null},
-{compId:"alarm2",room:"Room2",sNum:7,sWidth:20,sHeight:20,x:150,y:200,rotate:null},
-{compId:"door2",room:"Room2",sNum:8,sWidth:70,sHeight:5,x:395,y:260,rotate:-90},
-{compId:"stovetop3",room:"Room3",sNum:9,sWidth:40,sHeight:40,x:0,y:410,rotate:null},
-{compId:"pills3",room:"Room3",sNum:10,sWidth:20,sHeight:20,x:100,y:350,rotate:null},
-{compId:"alarm3",room:"Room3",sNum:11,sWidth:20,sHeight:20,x:150,y:350,rotate:null},
-{compId:"door3",room:"Room3",sNum:12,sWidth:70,sHeight:5,x:395,y:410,rotate:-90}
+{compId:"stovetop1",room:"Room1",sNum:1,sWidth:100,sHeight:100,x:0,y:200,rotate:null},
+{compId:"pills1",room:"Room1",sNum:2,sWidth:50,sHeight:50,x:300,y:200,rotate:null},
+{compId:"alarm1",room:"Room1",sNum:3,sWidth:50,sHeight:50,x:500,y:200,rotate:null},
+{compId:"door1",room:"Room1",sNum:4,sWidth:150,sHeight:10,x:740,y:200,rotate:-90},
+{compId:"stovetop2",room:"Room2",sNum:5,sWidth:100,sHeight:100,x:0,y:500,rotate:null},
+{compId:"pills2",room:"Room2",sNum:6,sWidth:50,sHeight:50,x:300,y:500,rotate:null},
+{compId:"alarm2",room:"Room2",sNum:7,sWidth:50,sHeight:50,x:500,y:500,rotate:null},
+{compId:"door2",room:"Room2",sNum:8,sWidth:150,sHeight:10,x:740,y:500,rotate:-90},
+{compId:"stovetop3",room:"Room3",sNum:9,sWidth:100,sHeight:100,x:0,y:800,rotate:null},
+{compId:"pills3",room:"Room3",sNum:10,sWidth:50,sHeight:50,x:300,y:800,rotate:null},
+{compId:"alarm3",room:"Room3",sNum:11,sWidth:50,sHeight:50,x:500,y:800,rotate:null},
+{compId:"door3",room:"Room3",sNum:12,sWidth:150,sHeight:10,x:740,y:800,rotate:-90},
+
+{compId:"stovetop4",room:"Room1",sNum:1,sWidth:100,sHeight:100,x:1750,y:200,rotate:null},
+{compId:"pills4",room:"Room1",sNum:2,sWidth:50,sHeight:50,x:1300,y:200,rotate:null},
+{compId:"alarm4",room:"Room1",sNum:3,sWidth:50,sHeight:50,x:1500,y:200,rotate:null},
+{compId:"door4",room:"Room1",sNum:4,sWidth:150,sHeight:10,x:1110,y:50,rotate:90},
+{compId:"stovetop5",room:"Room2",sNum:5,sWidth:100,sHeight:100,x:1750,y:500,rotate:null},
+{compId:"pills5",room:"Room2",sNum:6,sWidth:50,sHeight:50,x:1300,y:500,rotate:null},
+{compId:"alarm5",room:"Room2",sNum:7,sWidth:50,sHeight:50,x:1500,y:500,rotate:null},
+{compId:"door5",room:"Room2",sNum:8,sWidth:150,sHeight:10,x:1110,y:350,rotate:90},
+{compId:"stovetop6",room:"Room3",sNum:9,sWidth:100,sHeight:100,x:1750,y:800,rotate:null},
+{compId:"pills6",room:"Room3",sNum:10,sWidth:50,sHeight:50,x:1300,y:800,rotate:null},
+{compId:"alarm6",room:"Room3",sNum:11,sWidth:50,sHeight:50,x:1500,y:800,rotate:null},
+{compId:"door6",room:"Room3",sNum:12,sWidth:150,sHeight:10,x:1110,y:650,rotate:90},
+
+{compId:"door7",room:"Storage",sNum:13,sWidth:150,sHeight:10,x:850,y:600,rotate:0},
+{compId:"door8",room:"Entrance",sNum:14,sWidth:150,sHeight:10,x:850,y:0,rotate:0},
+
+{compId:"defibrillator",room:"Entrance",sNum:15,sWidth:50,sHeight:50,x:750,y:250,rotate:null},
+{compId:"first-aid",room:"Entrance",sNum:16,sWidth:50,sHeight:50,x:1050,y:250,rotate:null},
+{compId:"fire extinguisher",room:"Entrance",sNum:17,sWidth:50,sHeight:50,x:750,y:550,rotate:null},
+
+
     ];
-    
 
-
-  $scope.startX = 0;	
-  $scope.startY = 0;
-  $scope.mapWidth = 900;
-  $scope.mapHeight = 450;
-  
       
 });
