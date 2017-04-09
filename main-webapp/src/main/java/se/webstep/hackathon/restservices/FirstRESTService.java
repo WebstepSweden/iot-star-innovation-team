841c2c095c780b032d0af68f21662ab8584f009e
Fredrik Åhman <fredahman@gmail.com>
2017-04-09 09:59:37 +0200

    Added room view

--- main-webapp/src/main/java/se/webstep/hackathon/restservices/FirstRESTService.java
+++ main-webapp/src/main/java/se/webstep/hackathon/restservices/FirstRESTService.java
@@ -2,12 +2,21 @@ package se.webstep.hackathon.restservices;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
-@Path("/serviceTest")
+import se.webstep.hackathon.redis.RedisClient;
+
+@Path("/serviceTest/{subKey}")
 public class FirstRESTService {
+	
+	private RedisClient redisClient;
 
+	public FirstRESTService(){
+		redisClient=new RedisClient();
+	}
+	
 	@GET
-	public String testRestCall(){
-		return "funkar OK";
+	public String testRestCall(@PathParam("subKey") String subkey){
+		return redisClient.hamtaValue("tenant:1", 1);
 	}
 }
