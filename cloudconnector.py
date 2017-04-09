4fd0efa26bd4156f2f9bf83f68bc776f3d6fb396
root <root@localdomain>
2017-04-09 08:19:14 +0000

    Initial working POC

--- cloudconnector.py
+++ cloudconnector.py
@@ -4,8 +4,10 @@ import asyncio
 import aiohttp
 import os
 import json
-import redis
+import datetime
 from asyncio import async
+
+import redis
 
 
 # Global vars
@@ -17,9 +19,44 @@ headers = {
     'accept': 'text/event-stream',
 }
 
-# Redis setup
+
+# Global Redis setup
 rc = redis.Redis(decode_responses=True)
 our_ids = rc.smembers('thing_ids')
+
+
+# This should really be a separate coroutine instead (later on...)
+def add_redis(event):
+    # Determine type of sensor pressed
+    thing_key = 'thing_id:{}'.format(event.get('thing_id'))
+    _type = rc.hget(thing_key, 'type')
+    print(_type)
+
+    # Determine sensor location
+    location_key = rc.hget(thing_key, 'location_key')
+
+    # Update last updated
+    rc.hset(thing_key, 'updated', event.get('timestamp'))
+
+    # Specific characteristics for each sensor
+    # (touch applies for all sensors and only updates timestamp)
+    if _type == 'temp':
+        current_temp = event.get('state_changed', {}).get('temperature')
+        if current_temp is not None:
+            rc.hset(thing_key, 'current_temp', current_temp)
+            rc.publish(
+                'events',
+                '{}:current_temp:{}'.format(location_key, current_temp)
+            )
+    elif _type == 'proximity':
+        object_present = event.get('state_changed', {}).get('object_present')
+        if object_present is not None:
+            _open = int(not object_present)
+            rc.hset(thing_key, 'open', _open)
+            rc.publish('events', '{}:open:{}'.format(location_key, _open))
+    elif _type == 'touch':
+        rc.publish('events', location_key)
+
 
 async def fetch(session):
     print('Query {}'.format(dt_sub_ep))
@@ -37,7 +74,9 @@ async def fetch(session):
                 try:
                     event = json.loads(json_event).get('result')
                     if event.get('thing_id') in our_ids:
-                        print(event)
+                        # This should be added to a asyncio.Queue and consumed
+                        # by a separate coroutine instead
+                        add_redis(event)
                 except:
                     pass
 
